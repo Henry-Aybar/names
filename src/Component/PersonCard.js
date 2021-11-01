a1622833b2ceb7d3775be64533c872e78eb7466d
Henry Aybar <aybar.henry.usmc@gmail.com>
2021-11-01 15:10:45 -0500

    Usinf Functions not Class

--- src/Component/PersonCard.js
+++ src/Component/PersonCard.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: this.props.age,
-        };
-    }
-    birthday = () => {
-        this.setState({age : this.state.age + 1})
-    }
-
-    render(){
-        return(
-            <div>
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={this.birthday} > Make me older</button>
-            </div>
-        );
-    }
+//=========================
+// Using Function Component
+//=========================
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.FirstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    )
 }
+
+
+
+// //=======================
+// //  Using Class Componets
+// //=======================
+// class PersonCard extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             age: this.props.age,
+//         };
+//     }
+//     birthday = () => {
+//         this.setState({age : this.state.age + 1})
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
+//                 <p>Age: {this.state.age}</p>
+//                 <p>Hair Color: {this.props.hairColor}</p>
+//                 <button onClick={this.birthday} > Make me older</button>
+//             </div>
+//         );
+//     }
+// }
     
     export default PersonCard;
